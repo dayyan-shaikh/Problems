43c2faf8c4e53323fb8524bce2c61d1296f1eda5
Dayyan Shaikh <dayyanshaikh16@gmail.com>
2024-09-05 21:16:07 +0530

    commit

--- Javascript/containsduplicate.js
+++ Javascript/containsduplicate.js
@@ -1,3 +1,11 @@
-nums = [1,2,3,1]
+let nums = [1,2,3,2]
 
-let num = new Set()
+let num = new Set(nums)
+
+if(nums.length != num.size){
+    console.log("true");
+}else{
+    console.log("false");
+}
+    
+
